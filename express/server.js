ef8f50d1c92b4db0db752aab3c763c577653a672
ritesh <ssr@gmail.com>
2019-11-22 17:17:34 -0800

    changed server.js

--- express/server.js
+++ express/server.js
@@ -10,9 +10,20 @@ const mysql = require('mysql');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors())
+app.enable('trust proxy');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.use(express.static(__dirname + '/public'))
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
-app.use('/*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.use('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
