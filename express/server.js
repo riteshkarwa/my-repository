b80adc89de87ebcdd4088a8e1ce99276d7c397d1
ritesh <ssr@gmail.com>
2019-11-23 10:13:52 -0800

    test

--- express/server.js
+++ express/server.js
@@ -9,6 +9,11 @@ const mysql = require('mysql');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
