f3dfd1bd0b1801dab1eb0931ab7ba72b245668c2
ritesh <ssr@gmail.com>
2019-11-22 16:57:27 -0800

    changed server.js

--- express/server.js
+++ express/server.js
@@ -7,25 +7,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const mysql = require('mysql');
 
-/*
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.get('/*', function(request, response) {
-  //response.render('pages/index');
-  response.sendFile(__dirname +'/public/index.html');
-});
-//app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use('/*', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
