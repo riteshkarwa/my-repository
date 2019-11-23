92ab708cd0baa3127020caf6caf93d76dd61445a
ritesh <ssr@gmail.com>
2019-11-23 09:42:45 -0800

    change netlify.toml

--- express/server.js
+++ express/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const cors = require('cors')
 const mysql = require('mysql');
 
 
