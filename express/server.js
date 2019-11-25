966ba8ac0ff2ab77aa9e61583346adf5e665301c
ritesh <ssr@gmail.com>
2019-11-25 11:13:19 -0800

    added cors

--- express/server.js
+++ express/server.js
@@ -5,15 +5,19 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const cors = require('cors')
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+/*Enable CORS*/
+app.use(cors())
+app.enable('trust proxy');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // for local testing
 var connection = mysql.createConnection({
