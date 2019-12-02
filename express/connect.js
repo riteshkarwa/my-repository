8d148d964c09ffc1592e670d4deabebd956d1283
ritesh <ssr@gmail.com>
2019-12-02 12:21:53 -0800

    functions

--- express/connect.js
+++ express/connect.js
@@ -1,9 +1,6 @@
 const mysql = require('mysql');
 
-
-exports.handler = function (event, context, callback) {
-  
-  var connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host : "remotemysql.com",
     user: "XQu1tDNQ0B",
     database: "XQu1tDNQ0B",
@@ -11,29 +8,33 @@ exports.handler = function (event, context, callback) {
     //port:"3306"
   });
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error('Error connecting: ' + err.stack);
-      return {
-        statusCode: 400,
-        body: err
-      }
-    }
-    console.log('Connected as thread id: ' + connection.threadId);
-    var results=[];
-  
-    //SQL Query > Select Data
-    connection.query("SELECT * FROM num_of_likes", function(err, rows, fields) {
-      if (err) throw err;
-
-      for (var r in rows){
-        console.log(rows[r]);
-        results.push(rows[r]);
-      } 
-      return {
-            statusCode: 200,
-            body: results
-          }
-    });
-  });
+connection.connect(function(err) {
+if (err) {
+  console.error('Error connecting: ' + err.stack);
+  return {
+    statusCode: 400,
+    body: err
+  }
 }
+console.log('Connected as thread id: ' + connection.threadId);
+
+exports.handler = function (event, context, callback) {
+    if (connection.threadId){
+      var results=[];
+  
+      //SQL Query > Select Data
+      connection.query("SELECT * FROM num_of_likes", function(err, rows, fields) {
+        if (err) throw err;
+
+        for (var r in rows){
+          console.log(rows[r]);
+          results.push(rows[r]);
+        } 
+        callback(null,{
+              statusCode: 200,
+              body: JSON.stringify(results)
+            })
+      });
+    }
+  }
+
