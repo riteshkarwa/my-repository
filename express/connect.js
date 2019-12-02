acfd014a0e73176065b288c92732b1c31d5ea5db
ritesh <ssr@gmail.com>
2019-12-02 15:27:19 -0800

    likes

--- express/connect.js
+++ express/connect.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
-let  conn = null;
-
-var connection = mysql.createConnection({
-    host : "remotemysql.com",
-    user: "XQu1tDNQ0B",
-    database: "XQu1tDNQ0B",
-    password: "xNwMQh7jQ8"
-    //port:"3306"
-  });
 
 
 exports.handler = function (event, context, callback) {
+
+    var connection = mysql.createConnection({
+      host : "remotemysql.com",
+      user: "XQu1tDNQ0B",
+      database: "XQu1tDNQ0B",
+      password: "xNwMQh7jQ8"
+      //port:"3306"
+    });
+
   
     connection.connect(function(err) {
     if (err) {
@@ -18,12 +18,12 @@ exports.handler = function (event, context, callback) {
     }
     console.log('Connected as thread id: ' + connection.threadId);
     });
-    
     var results=[];
     //SQL Query > Select Data
     connection.query("SELECT * FROM num_of_likes", function(err, rows, fields) {
-      if (err) throw err;
-
+      if (err) {
+        console.log(err);
+      }
       for (var r in rows){
         console.log(rows[r]);
         results.push(rows[r]);
@@ -33,7 +33,7 @@ exports.handler = function (event, context, callback) {
         body: JSON.stringify(results)
       }
     });
-};
+}
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -72,68 +72,80 @@ var scotchApp = angular.module('myApp', ['ngRoute','ui.bootstrap']);
             {
             id: 1,
             url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885824/frenchstand_dtiyhu.jpg",
-            title: "French Provincial Night Stand"
+            title: "French Provincial Night Stand",
+            likes: 100
             },
             {
                 id: 2,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882123/stand_mkbg5e.jpg",
-                title: "Adorable Side Table"
+                title: "Adorable Side Table",
+                likes: 130
             },
             {
                 id: 3,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885212/midcentury_kxvnrs.jpg",
-                title: "Awesome Mid Century Table"
+                title: "Awesome Mid Century Table",
+                likes: 150
             },
             {
                 id: 4,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1474155614/2tier_oldgo9.jpg",
-                title: "French Provincial End Table"
+                title: "French Provincial End Table",
+                likes: 200
             },
             {
                 id: 5,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882106/doggy_bed_u3jign.jpg",
-                title: "Doggy Bed"
+                title: "Doggy Bed",
+                likes: 190
             },
             {
                 id: 6,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463881943/breadbox_gxioq9.jpg",
-                title: "Bread Box"
+                title: "Bread Box",
+                likes: 300
             },
             {   
                 id:7,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1473479543/image_1_kzwonl.jpg",
-                title: "Piano Bench"
+                title: "Piano Bench",
+                likes: 400
             },
             {
                 id:8,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1480115054/coffee_table_fpbrud.jpg",
-                title: "Coffee Table"
+                title: "Coffee Table",
+                likes: 250
             },
             {
                 id:9,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1511933009/24251248_10211316135001007_1093863881_o_ezqau5.jpg",
-                title: "Radiant Prayer/Dinning Table"
+                title: "Radiant Prayer/Dinning Table",
+                likes: 200
             },
             {
                 id:10,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/q_100/v1512889763/25198672_10211398496099983_340813110_o_k5fxgk.jpg",
-                title: "Solid Cherry Night Stands"
+                title: "Solid Cherry Night Stands",
+                likes: 520
             },
             {
                 id:11,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1524437998/31166904_10212423764531053_3514226770838552576_n_osyrmh.jpg",
-                title: "Solid Wood Study/Work Table"
+                title: "Solid Wood Study/Work Table",
+                likes: 600
             },
             {
                 id:12,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1531019940/36772328_10212926705784270_2315435320453103616_n_cgn34y.jpg",
-                title: "Gorgeous Uphostered Bench"
+                title: "Gorgeous Uphostered Bench",
+                likes: 500
             }
         ]
 
-        fetch('/.netlify/functions/connect')
+        fetch('/api/all_likes/')
         .then(res =>{
-            console.log(res.json());
+            console.log(res);
         
         })
         //Get all likes 
