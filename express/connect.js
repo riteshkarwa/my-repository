c4be0ddb239101c8b57301a5638aac42c6310cb3
rkarw0212 <rkarw0212@AHUSB0610-L1.local>
2020-01-23 10:38:51 -0800

    mysql connection

--- express/connect.js
+++ express/connect.js
@@ -1,57 +1,93 @@
+// const mysql = require('mysql');
+
+// if (typeof connection === 'undefined'){
+
+//   //Connect to mysql database
+//   var connection = mysql.createConnection({
+//       host : "sql3.freemysqlhosting.net",
+//       user: "sql3315408",
+//       database: "sql3315408",
+//       password: "zxLX9aF7ID",
+//       //port:"3306"
+//     });
+
+//   connection.connect(function(err) {
+//       if (err) {
+//         console.error('Error connecting: ' + err.stack);
+//       }
+//       console.log('Connected as thread id: ' + connection.threadId);
+//   });
+// }
+
+// exports.handler = function (event, context, callback) {
+    
+//     context.callbackWaitsForEmptyEventLoop =false;
+
+//     var results=[];
+//     //SQL Query > Select Data
+
+//     // return new Promise((resolve, reject) => {
+//     //   const readTable = `SELECT * FROM num_of_likes`;
+//     //   connection.query(readTable, (err, results, fields) => {
+//     //     if (err) {
+//     //      reject(err);
+//     //     } else {
+//     //       resolve({statusCode: 200, body: {results}});
+//     //     }
+//     //   });
+//     // });
+
+//     connection.query("SELECT * FROM num_of_likes", function(err, rows, fields) {
+//       if (err) {
+//         console.log(err);
+//       }
+//       for (var r in rows){
+//         console.log(rows[r]);
+//         results.push(rows[r]);
+//       } 
+//       callback(null, {
+//         statusCode: 200,
+//         body: JSON.stringify(results)
+//       })
+//     }); 
+    
+// }
+
+
+
+
 const mysql = require('mysql');
-
-if (typeof connection === 'undefined'){
-
-  //Connect to mysql database
-  var connection = mysql.createConnection({
+ 
+exports.handler = (event, context, callback) => {
+  const connection = mysql.createConnection({
       host : "sql3.freemysqlhosting.net",
       user: "sql3315408",
       database: "sql3315408",
       password: "zxLX9aF7ID",
-      //port:"3306"
-    });
-
-  connection.connect(function(err) {
-      if (err) {
-        console.error('Error connecting: ' + err.stack);
-      }
-      console.log('Connected as thread id: ' + connection.threadId);
   });
-}
-
-exports.handler = function (event, context, callback) {
-    
-    context.callbackWaitsForEmptyEventLoop =false;
-
-    var results=[];
-    //SQL Query > Select Data
-
-    // return new Promise((resolve, reject) => {
-    //   const readTable = `SELECT * FROM num_of_likes`;
-    //   connection.query(readTable, (err, results, fields) => {
-    //     if (err) {
-    //      reject(err);
-    //     } else {
-    //       resolve({statusCode: 200, body: {results}});
-    //     }
-    //   });
-    // });
-
-    connection.query("SELECT * FROM num_of_likes", function(err, rows, fields) {
-      if (err) {
-        console.log(err);
-      }
-      for (var r in rows){
-        console.log(rows[r]);
-        results.push(rows[r]);
-      } 
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(results)
-      })
-    }); 
-    
-}
+ 
+  connection.connect();
+  
+  connection.query('SELECT * FROM num_of_likes', (error, results) => {
+    if (error) {
+      console.log('fail:');
+      console.log(error);
+      callback(error, null);
+    } else {
+      console.log('success:');
+      console.log(results);
+      connection.end((err) => {
+        if (err) {
+          console.log('fail:');
+          console.log(err);
+          callback(err, null);
+        } else {
+          callback(null, results);
+        }
+      });
+    }
+  });
+};
 
 
 
