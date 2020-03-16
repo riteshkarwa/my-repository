7615cbeb272b6a2f3a4f75a213fe03ab231dd59c
rkarw0212 <rkarw0212@AHUSB0610-L1.attlocal.net>
2020-03-16 00:26:02 -0700

    add update function

---
+++ express/update_likes.js
@@ -0,0 +1,29 @@
+const faunadb = require('faunadb')
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: 'fnADnAs2ygACCgKqaUyLxaAMPWfR8O8KWEy3DPmB'
+})
+
+function getId(urlPath) {
+    return urlPath.match(/([^\/]*)\/*$/)[0]
+}
+
+exports.handler = (event, context) => {
+    const data = JSON.parse(event.body)
+    const id = getId(event.path)
+    console.log(`Function 'like-update' invoked. update id: ${id}`)
+    return client.query(q.Update(q.Ref(`num_of_likes/${id}`), {data}))
+      .then((response) => {
+        console.log('success', response)
+        return {
+          statusCode: 200,
+          body: JSON.stringify(response)
+        }
+    }).catch((error) => {
+        console.log('error', error)
+        return {
+          statusCode: 400,
+          body: JSON.stringify(error)
+        }
+    })
+}
