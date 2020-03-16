37b3042a15f471a17735202e6ea4e9f04823d362
rkarw0212 <rkarw0212@AHUSB0610-L1.attlocal.net>
2020-03-15 18:37:47 -0700

    remove likes

--- public/js/main.js
+++ public/js/main.js
@@ -82,62 +82,52 @@ var scotchApp = angular.module('myApp', ['ngRoute','ui.bootstrap']);
             {
                 id: 3,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885212/midcentury_kxvnrs.jpg",
-                title: "Awesome Mid Century Table",
-                likes: 150
+                title: "Awesome Mid Century Table"
             },
             {
                 id: 4,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1474155614/2tier_oldgo9.jpg",
-                title: "French Provincial End Table",
-                likes: 200
+                title: "French Provincial End Table"
             },
             {
                 id: 5,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882106/doggy_bed_u3jign.jpg",
-                title: "Doggy Bed",
-                likes: 190
+                title: "Doggy Bed"
             },
             {
                 id: 6,
                 url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463881943/breadbox_gxioq9.jpg",
-                title: "Bread Box",
-                likes: 180
+                title: "Bread Box"
             },
             {   
                 id:7,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1473479543/image_1_kzwonl.jpg",
-                title: "Piano Bench",
-                likes: 135
+                title: "Piano Bench"
             },
             {
                 id:8,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1480115054/coffee_table_fpbrud.jpg",
-                title: "Coffee Table",
-                likes: 250
+                title: "Coffee Table"
             },
             {
                 id:9,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1511933009/24251248_10211316135001007_1093863881_o_ezqau5.jpg",
-                title: "Radiant Prayer/Dinning Table",
-                likes: 260
+                title: "Radiant Prayer/Dinning Table"
             },
             {
                 id:10,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/q_100/v1512889763/25198672_10211398496099983_340813110_o_k5fxgk.jpg",
-                title: "Solid Cherry Night Stands",
-                likes: 125
+                title: "Solid Cherry Night Stands"
             },
             {
                 id:11,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1524437998/31166904_10212423764531053_3514226770838552576_n_osyrmh.jpg",
-                title: "Solid Wood Study/Work Table",
-                likes: 210
+                title: "Solid Wood Study/Work Table"
             },
             {
                 id:12,
                 url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1531019940/36772328_10212926705784270_2315435320453103616_n_cgn34y.jpg",
-                title: "Gorgeous Uphostered Bench",
-                likes: 290
+                title: "Gorgeous Uphostered Bench"
             }
         ]
         
