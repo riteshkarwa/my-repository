d72e3c9fc03f8038d6bd4e424ec02585d9d3bea9
rkarw0212 <rkarw0212@AHUSB0610-L1.attlocal.net>
2020-03-16 13:11:58 -0700

    fix PUT request

--- public/js/main.js
+++ public/js/main.js
@@ -200,7 +200,7 @@ var scotchApp = angular.module('myApp', ['ngRoute','ui.bootstrap']);
                     $http({
                         method: 'PUT',
                         url: "/.netlify/functions/update_likes/" + image.id,
-                        data: JSON.stringify({num_of_likes:image.likes}),
+                        data: JSON.stringify({"num_of_likes":image.likes}),
                         headers: {
                             'Content-Type': 'application/json'
                         }
