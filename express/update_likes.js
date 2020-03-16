2be8be55a655a70341449d87734d5e18d4ac13bb
rkarw0212 <rkarw0212@AHUSB0610-L1.attlocal.net>
2020-03-16 11:27:37 -0700

    new code

--- express/update_likes.js
+++ express/update_likes.js
@@ -4,17 +4,13 @@ const client = new faunadb.Client({
   secret: 'fnADnAs2ygACCgKqaUyLxaAMPWfR8O8KWEy3DPmB'
 })
 
-function getId(urlPath) {
-    return urlPath.match(/([^\/]*)\/*$/)[0]
-}
-
 exports.handler = (event, context, callback) => {
     const data = JSON.parse(event.body);
     console.log(data);
-    const id = getId(event.path)
+    const id = event.id
     console.log(id);
     console.log(`Function 'like-update' invoked. update id: ${id}`)
-    return client.query(q.Update(q.Ref(`num_of_likes/${id}`), {data}))
+    return client.query(q.Update(q.Ref(`classes/num_of_likes/${id}`), {data}))
     .then((response) => {
       console.log("success", response)
       return callback(null, {
