1a4851696dbb702a3b5033825aabfbd4b5de3921
rkarw0212 <rkarw0212@AHUSB0610-L1.attlocal.net>
2020-03-16 18:03:22 -0700

    extract ref using index

--- express/update_likes.js
+++ express/update_likes.js
@@ -8,27 +8,25 @@ function getId(urlPath) {
     return urlPath.match(/([^\/]*)\/*$/)[0]
 }
 exports.handler = (event, context, callback) => {
-    const data = JSON.parse(event.body);
-    console.log(data);
+    //const data = JSON.parse(event.body);
+    //console.log(event.body);
     const id = getId(event.path)
     console.log(id);
     console.log(`Function 'like-update' invoked. update id: ${id}`)
-    client.query(q.Get(q.Match(q.Index('search_by_id'), `${id}`)))
+    client.query(q.Paginate(q.Match(q.Index("search_by_id"), Number(`${id}`))))
       .then((ret) => {
-        const searchRefs = response.data
-        console.log("Likes refs", searchRefs)
-        alert(searchRefs)
-        const dataRef = searchRefs.map((ref) => {
-            return q.Get(ref)
-        })
-        return client.query(q.Update(q.Ref(q.Collection("num_of_likes"),`${dataRef}`), {data}))
+        const searchRefs = ret.data
+        
+        const datastr= JSON.stringify(searchRefs);
+        const dataRef = JSON.parse(datastr)[0]['@ref']['id'];
+        return client.query(q.Update(q.Ref(q.Collection("num_of_likes"),Number(dataRef)), {data}))
         .then((response) => {
-        console.log("success", response)
-        return callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(response)
+            console.log("success", response)
+            return callback(null, {
+                statusCode: 200,
+                body: JSON.stringify(response)
+            })
         })
-      })
     }).catch((error) => {
       console.log("error", error)
       return callback(null, {
