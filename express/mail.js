a151c68ab14055e94623be7073fbfee51748b62e
rkarw0212 <rkarw0212@AHUSB0610-L1.attlocal.net>
2020-03-17 20:23:31 -0700

    more test code

--- express/mail.js
+++ express/mail.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = (event, context, callback) => {
-    const data = JSON.parse(event.body);
-    console.log(event.body);
+    //const data = JSON.parse(event.body);
+    console.log(event.body.name);
 
     // Here we are configuring our SMTP Server details.
     // STMP is mail server which is responsible for sending and recieving email.
@@ -17,18 +17,26 @@ exports.handler = (event, context, callback) => {
 
     console.log("in mail controller");
     var mailOptions={
-        from : data.name + " " + data.email + " ",
+        from : event.body.name + " " + event.body.email + " ",
         to : process.env.EMAIL,
-        subject : data.subject,
-        text : data.text
+        subject : event.body.subject,
+        text : event.body.text
     }
     console.log(mailOptions);
 
     smtpTransport.sendMail(mailOptions, function(error, response){
         if(error){
           console.log(error);
+          return callback(null, {
+            statusCode: 400,
+            body: JSON.stringify(error)
+          })
         }else{
           console.log("Message sent: " + response.message);
+          return callback(null, {
+            statusCode: 200,
+            body: response.message
+          })
         }
     });
 
