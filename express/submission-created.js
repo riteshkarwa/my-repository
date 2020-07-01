30c71fa6e5fe3418b877f0f20082f8941e574817
ritesh <riteshkarwa@yahoo.com>
2020-06-30 20:02:30 -0700

    fix the pinterest pin

--- express/submission-created.js
+++ express/submission-created.js
@@ -1,37 +1,36 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 exports.handler = (event, context, callback) => {
-    const data = JSON.parse(event.body)
-    console.log(data);
+  const data = JSON.parse(event.body);
+  console.log(data);
 
-    // Here we are configuring our SMTP Server details.
-    // STMP is mail server which is responsible for sending and recieving email.
+  // Here we are configuring our SMTP Server details.
+  // STMP is mail server which is responsible for sending and recieving email.
 
-    var smtpTransport = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: process.env.EMAIL,
-          pass: process.env.PASSWORD
-        }
-    });
+  var smtpTransport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
+  let mailOptions = {
+    from: data.email,
+    to: process.env.EMAIL,
+    subject: data.subject,
+    text: `This message is from:${data.name} Message:${data.text}`,
+  };
+  console.log(mailOptions);
 
-    let mailOptions={
-        from: data.email,
-        to: process.env.EMAIL,
-        subject: data.subject,
-        text: `This message is from:${data.name} Message:${data.text}`
+  smtpTransport.sendMail(mailOptions, function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Message %s sent: %s", response.messageId, response.response);
     }
-    console.log(mailOptions);
-
-    smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-          console.log(error);
-        }else{
-            console.log('Message %s sent: %s', response.messageId, response.response);
-        }
-    });
-}
+  });
+};
