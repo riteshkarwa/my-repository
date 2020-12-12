0e45ecb76fdb60d59264d945f426f5a3e0563d18
ritesh <riteshkarwa@yahoo.com>
2020-12-12 13:05:32 -0800

    add donationController

--- public/js/main.js
+++ public/js/main.js
@@ -1,213 +1,233 @@
-var scotchApp = angular.module('myApp', ['ngRoute','ui.bootstrap']);
+var scotchApp = angular.module("myApp", ["ngRoute", "ui.bootstrap"]);
 
-    // create the controller and inject Angular's $scope
+// create the controller and inject Angular's $scope
 
-		// configure our routes
-        scotchApp.config(function($routeProvider,$locationProvider) {
-        $routeProvider
+// configure our routes
+scotchApp.config(function ($routeProvider, $locationProvider) {
+  $routeProvider
 
-            // route for the home page
-            .when('/', {
-                templateUrl : 'home.html',
-                controller  : 'mainController'
-            })
+    // route for the home page
+    .when("/", {
+      templateUrl: "home.html",
+      controller: "mainController",
+    })
 
-            //route for blogs
-            .when('/blog', {
-                templateUrl : 'blog.html',
-                controller  : 'blogController'
-            })
+    //route for blogs
+    .when("/blog", {
+      templateUrl: "blog.html",
+      controller: "blogController",
+    })
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'about.html',
-                controller  : 'aboutController'
-            })
+    // route for the about page
+    .when("/about", {
+      templateUrl: "about.html",
+      controller: "aboutController",
+    })
 
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'contact.html',
-                controller  : 'contactController'
-            })
+    // route for the contact page
+    .when("/contact", {
+      templateUrl: "contact.html",
+      controller: "contactController",
+    })
 
-            // route for the gallery page
-            .when('/gallery', {
-                templateUrl : 'gallery.html',
-                controller  : 'galleryController'
-            })
-            // route for the terms of use page
-            .when('/terms', {
-                templateUrl : 'terms.html',
-                controller  : 'termsController'
-            });
+    // route for the gallery page
+    .when("/gallery", {
+      templateUrl: "gallery.html",
+      controller: "galleryController",
+    })
 
-        // use the HTML5 History API
-        $locationProvider.html5Mode(true);
+    // route for the donate page
+    .when("/gallery", {
+      templateUrl: "donate.html",
+      controller: "donateController",
+    })
+    // route for the terms of use page
+    .when("/terms", {
+      templateUrl: "terms.html",
+      controller: "termsController",
     });
 
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
+  // use the HTML5 History API
+  $locationProvider.html5Mode(true);
+});
 
-    scotchApp.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
-    });
+// create the controller and inject Angular's $scope
+scotchApp.controller("mainController", function ($scope) {
+  // create a message to display in our view
+  $scope.message = "Everyone come and see how good I look!";
+});
 
-    scotchApp.controller('blogController', function($scope) {
-        $scope.message = 'Look! I am a blog page.';
-    });
+scotchApp.controller("aboutController", function ($scope) {
+  $scope.message = "Look! I am an about page.";
+});
 
-    scotchApp.controller('termsController', function($scope) {
-        $scope.message = 'Look! I am a terms of use page.';
-    });
+scotchApp.controller("blogController", function ($scope) {
+  $scope.message = "Look! I am a blog page.";
+});
 
-    scotchApp.controller('galleryController', function($scope,$http) 
+scotchApp.controller("termsController", function ($scope) {
+  $scope.message = "Look! I am a terms of use page.";
+});
+
+scotchApp.controller("galleryController", function ($scope, $http) {
+  $scope.counts = [];
+  $scope.results = [];
+
+  $scope.images = [
     {
+      id: 1,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885824/frenchstand_dtiyhu.jpg",
+      title: "French Provincial Night Stand",
+    },
+    {
+      id: 2,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882123/stand_mkbg5e.jpg",
+      title: "Adorable Side Table",
+    },
+    {
+      id: 3,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885212/midcentury_kxvnrs.jpg",
+      title: "Awesome Mid Century Table",
+    },
+    {
+      id: 4,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1474155614/2tier_oldgo9.jpg",
+      title: "French Provincial End Table",
+    },
+    {
+      id: 5,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882106/doggy_bed_u3jign.jpg",
+      title: "Doggy Bed",
+    },
+    {
+      id: 6,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463881943/breadbox_gxioq9.jpg",
+      title: "Bread Box",
+    },
+    {
+      id: 7,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1473479543/image_1_kzwonl.jpg",
+      title: "Piano Bench",
+    },
+    {
+      id: 8,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1480115054/coffee_table_fpbrud.jpg",
+      title: "Coffee Table",
+    },
+    {
+      id: 9,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1511933009/24251248_10211316135001007_1093863881_o_ezqau5.jpg",
+      title: "Radiant Prayer/Dinning Table",
+    },
+    {
+      id: 10,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/q_100/v1512889763/25198672_10211398496099983_340813110_o_k5fxgk.jpg",
+      title: "Solid Cherry Night Stands",
+    },
+    {
+      id: 11,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1524437998/31166904_10212423764531053_3514226770838552576_n_osyrmh.jpg",
+      title: "Solid Wood Study/Work Table",
+    },
+    {
+      id: 12,
+      url:
+        "https://res.cloudinary.com/dkjcddqy0/image/upload/v1531019940/36772328_10212926705784270_2315435320453103616_n_cgn34y.jpg",
+      title: "Gorgeous Uphostered Bench",
+    },
+  ];
 
-        $scope.counts = [];
-        $scope.results = [];
+  // $http.get('/.netlify/functions/server.js')
+  // .then(res=>{
+  //     console.log(res);
+  // })
 
-        $scope.images = [
-            {
-                id: 1,
-                url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885824/frenchstand_dtiyhu.jpg",
-                title: "French Provincial Night Stand"
-            },
-            {
-                id: 2,
-                url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882123/stand_mkbg5e.jpg",
-                title: "Adorable Side Table"
-            },
-            {
-                id: 3,
-                url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463885212/midcentury_kxvnrs.jpg",
-                title: "Awesome Mid Century Table"
-            },
-            {
-                id: 4,
-                url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1474155614/2tier_oldgo9.jpg",
-                title: "French Provincial End Table"
-            },
-            {
-                id: 5,
-                url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463882106/doggy_bed_u3jign.jpg",
-                title: "Doggy Bed"
-            },
-            {
-                id: 6,
-                url: "https://res.cloudinary.com/dkjcddqy0/image/upload/v1463881943/breadbox_gxioq9.jpg",
-                title: "Bread Box"
-            },
-            {   
-                id:7,
-                url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1473479543/image_1_kzwonl.jpg",
-                title: "Piano Bench"
-            },
-            {
-                id:8,
-                url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1480115054/coffee_table_fpbrud.jpg",
-                title: "Coffee Table"
-            },
-            {
-                id:9,
-                url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1511933009/24251248_10211316135001007_1093863881_o_ezqau5.jpg",
-                title: "Radiant Prayer/Dinning Table"
-            },
-            {
-                id:10,
-                url:"https://res.cloudinary.com/dkjcddqy0/image/upload/q_100/v1512889763/25198672_10211398496099983_340813110_o_k5fxgk.jpg",
-                title: "Solid Cherry Night Stands"
-            },
-            {
-                id:11,
-                url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1524437998/31166904_10212423764531053_3514226770838552576_n_osyrmh.jpg",
-                title: "Solid Wood Study/Work Table"
-            },
-            {
-                id:12,
-                url:"https://res.cloudinary.com/dkjcddqy0/image/upload/v1531019940/36772328_10212926705784270_2315435320453103616_n_cgn34y.jpg",
-                title: "Gorgeous Uphostered Bench"
-            }
-        ]
-        
-        // $http.get('/.netlify/functions/server.js')
-        // .then(res=>{
-        //     console.log(res);
-        // })
+  // fetch('/.netlify/functions/all_likes')
+  // .then((resp) => resp.json())
+  // .then(function(data) {
+  //     for (var r in data){
+  //         $scope.results.push(data[r]['data']);
+  //     }
+  // }).catch((error) => {
+  //     console.log("error", error)
+  // })
+  // console.log($scope.results);
+  // $scope.images.forEach(function(img){
+  //     $scope.results.forEach(function(like){
+  //         if (img.id == like.id) {
+  //             img.likes = like.num_of_likes;
+  //         } else if (!img.likes) {
+  //             img.likes = 0;
+  //         }
+  //     });
+  // });
 
-        // fetch('/.netlify/functions/all_likes')
-        // .then((resp) => resp.json())
-        // .then(function(data) {
-        //     for (var r in data){
-        //         $scope.results.push(data[r]['data']);
-        //     }
-        // }).catch((error) => {
-        //     console.log("error", error)
-        // })
-        // console.log($scope.results);
-        // $scope.images.forEach(function(img){
-        //     $scope.results.forEach(function(like){
-        //         if (img.id == like.id) {
-        //             img.likes = like.num_of_likes;
-        //         } else if (!img.likes) {
-        //             img.likes = 0;
-        //         }
-        //     });
-        // });
-
-        // //Get all likes 
-        $http.get('/.netlify/functions/all_likes')
-        .success(function(likes) {
-            //console.log(likes);
-            $scope.images.forEach(function(img){
-                likes.forEach(function(like){
-                    if (img.id == like['data'].id) {
-                        img.likes = like['data'].num_of_likes;
-                    } else if (!img.likes) {
-                        img.likes = 0;
-                    }
-                })
-            })
-
-        }).error(function(error) {
-            console.log('Error: ' + error);
+  // //Get all likes
+  $http
+    .get("/.netlify/functions/all_likes")
+    .success(function (likes) {
+      //console.log(likes);
+      $scope.images.forEach(function (img) {
+        likes.forEach(function (like) {
+          if (img.id == like["data"].id) {
+            img.likes = like["data"].num_of_likes;
+          } else if (!img.likes) {
+            img.likes = 0;
+          }
         });
-
-        $scope.favorite = function(image) {
-            //console.log(image.id);
-            if (!image.been_liked) {
-                    image.likes += 1;    
-                    image.been_liked = true;
-                    fetch(`/.netlify/functions/update_likes/${image.id}`, {
-                        body: JSON.stringify({num_of_likes:image.likes}),
-                        method: 'PUT'
-                    }).then(response => {
-                        response.json().then(function(data) {
-                            console.log(data);
-                        })
-                    }).catch((error) => {
-                        console.log("error", error)
-                    })
-                } 
-            else {
-                image.likes -= 1;
-                fetch(`/.netlify/functions/update_likes/${image.id}`, {
-                    body: JSON.stringify({num_of_likes:image.likes}),
-                    method: 'PUT'
-                }).then(response => {
-                    response.json().then(function(data) {
-                        console.log(data);
-                    })
-                }).catch((error) => {
-                    console.log("error", error)
-                })
-                image.been_liked = false;
-            }
-        }  
+      });
+    })
+    .error(function (error) {
+      console.log("Error: " + error);
     });
 
-    scotchApp.controller('contactController', function($scope) {
-        $scope.message = 'Contact me at chelseafelter@yahoo.com';
-    });
+  $scope.favorite = function (image) {
+    //console.log(image.id);
+    if (!image.been_liked) {
+      image.likes += 1;
+      image.been_liked = true;
+      fetch(`/.netlify/functions/update_likes/${image.id}`, {
+        body: JSON.stringify({ num_of_likes: image.likes }),
+        method: "PUT",
+      })
+        .then((response) => {
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+    } else {
+      image.likes -= 1;
+      fetch(`/.netlify/functions/update_likes/${image.id}`, {
+        body: JSON.stringify({ num_of_likes: image.likes }),
+        method: "PUT",
+      })
+        .then((response) => {
+          response.json().then(function (data) {
+            console.log(data);
+          });
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+      image.been_liked = false;
+    }
+  };
+});
+
+scotchApp.controller("contactController", function ($scope) {
+  $scope.message = "Contact me at chelseafelter@yahoo.com";
+});
